9a3bedd64d23913147e38c60f8142dff492ea5fe
seunghyun711 <seunghyun711@gmail.com>
2023-02-07 16:50:52 +0900

    싱글톤 패턴의 문제를 싱글톤 컨테이너로 해결

--- src/main/java/com/example/dipractice/DependencyConfig.java
+++ src/main/java/com/example/dipractice/DependencyConfig.java
@@ -4,22 +4,29 @@ import com.example.dipractice.championships.ChampionShipRepository;
 import com.example.dipractice.championships.ChampionsShipsService;
 import com.example.dipractice.team.TeamRepository;
 import com.example.dipractice.team.TeamService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 의존성 주입을 관리하는 클래스
+@Configuration
 public class DependencyConfig {
 
+    @Bean
     public TeamService teamService(){
         return new TeamService(teamRepository());
     }
 
+    @Bean
     public TeamRepository teamRepository(){
         return new TeamRepository();
     }
 
+    @Bean
     public ChampionsShipsService championsShipsService(){
         return new ChampionsShipsService(championShipRepository());
     }
 
+    @Bean
     public ChampionShipRepository championShipRepository(){
         return new ChampionShipRepository();
     }
--- src/main/java/com/example/dipractice/SingletonTest.java
+++ src/main/java/com/example/dipractice/SingletonTest.java
@@ -2,14 +2,17 @@ package com.example.dipractice;
 
 import com.example.dipractice.singleton.SingletonService;
 import com.example.dipractice.team.TeamService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
-    // 싱글톤 적용 o
-    static SingletonService singletonService1 = SingletonService.getInstance();
-    static SingletonService singletonService2 = SingletonService.getInstance();
+    // 싱글톤 적용 o(컨테이너 사용)
+
+    static AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(DependencyConfig.class);
+    static TeamService teamService1 = ac.getBean("teamService", TeamService.class);
+    static TeamService teamService2 = ac.getBean("teamService", TeamService.class);
 
     public static void main(String[] args) {
-        System.out.println("singletonService1 : " + singletonService1);
-        System.out.println("singletonService2 : " + singletonService2);
+        System.out.println("teamService1 : " + teamService1);
+        System.out.println("teamService2 : " + teamService2);
     }
 }
